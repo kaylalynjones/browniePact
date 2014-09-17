ed613305d26c169dbbdaf4dd9a6c36d76d2d4dc3
Kayla Jones <KaylaLyne424@gmail.com>
2014-09-16 22:49:15 -0500

    css front page

--- client/views/dashboards/dashboards.js
+++ client/views/dashboards/dashboards.js
@@ -2,10 +2,44 @@
   'use strict';
 
   angular.module('browniePact')
-  .controller('DashboardsCtrl', ['$scope', function($scope){
+  .controller('DashboardsCtrl', ['$scope', 'Food', 'Exercise', function($scope, Food, Exercise){
+
+    $scope.foods = [];
+    $scope.food = {};
+    $scope.exercises = [];
+    $scope.exercise ={};
+    $scope.activities = [];
+    $scope.date = new Date();
+    $scope.exerciseCalories = [];
+
+    $scope.addFood = function(){
+      Food.create($scope.food).then(function(response){
+        $scope.foods.push(response.data.food);
+        $scope.food = {};
+      });
+    };
+
+    Food.all().then(function(response){
+      $scope.foods = response.data.foods;
+    });
+
+    $scope.addExercise = function(){
+      Exercise.create($scope.exercise).then(function(response){
+        $scope.exercises.push(response.data.exercise);
+        $scope.exercise = {};
+      });
+    };
+
+    Exercise.all().then(function(response){
+      $scope.exercises = response.data.exercises;
+      
+    });
+
+    $scope.totalCalories = function(){
+      Exercise.all().then
+    };
 
     $scope.config ={
-      debugger;
       title: 'Calorie Intake',
       tooltips: false,
       labels: true,
